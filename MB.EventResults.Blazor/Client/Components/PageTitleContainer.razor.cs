6362e685210dc6dbc9164366825b9b50cd91e743
Magnus Bengtsson <nzmangan@hotmail.com>
2022-06-02 17:40:05 +1200

    Mobile layout.

--- MB.EventResults.Blazor/Client/Components/PageTitleContainer.razor.cs
+++ MB.EventResults.Blazor/Client/Components/PageTitleContainer.razor.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace MB.EventResults.Blazor.Client.Components;
+﻿namespace MB.EventResults.Blazor.Client.Components;
 
 public partial class PageTitleContainer {
   [Parameter]
   public string Title { get; set; }
+
+  [Parameter]
+  public string Byline { get; set; }
 
   [Parameter]
   public RenderFragment ChildContent { get; set; }
